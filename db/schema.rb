c642cec744bd30eb89ddd1d290ebb584fe2c5b8e
Jay Pinho <jaypinho@gmail.com>
2015-08-17 19:10:59 -0400

    Switch to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150817225956) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "projections", force: :cascade do |t|
     t.date     "present_date"
     t.decimal  "projected_rate"
