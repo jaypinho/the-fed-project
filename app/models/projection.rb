d6d1b1c836d41524ac69d57f61f9faf1375c1589
Jay Pinho <jaypinho@gmail.com>
2015-08-20 22:08:33 -0700

    Use actual Fed funds rate

--- app/models/projection.rb
+++ app/models/projection.rb
@@ -1,17 +1,2 @@
 class Projection < ActiveRecord::Base
-
-  def self.read_events
-
-    require 'open-uri'
-    require 'net/http'
-    require 'csv'
-
-    uri = URI("http://jaypinho.com/fedfunds.csv")
-    csv_text = Net::HTTP.get(uri)
-    csv = CSV.parse(csv_text)
-    csv.each do |row|
-      KeyRate.create(:present_date => row[0].to_date, :actual_rate => row[1].to_d)
-    end
-  end
-
 end
