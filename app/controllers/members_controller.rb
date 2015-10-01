700a2076ffcd064dba2070eb13a3c4d7a06d32e7
Jay Pinho <jaypinho@gmail.com>
2015-10-01 11:50:52 -0400

    Add membership functionality

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,52 @@
+class MembersController < ApplicationController
+
+before_action :set_member, only: [:show, :edit, :update, :destroy]
+
+def index
+end
+
+def show
+end
+
+def new
+  @member = Member.new
+end
+
+def edit
+end
+
+def create
+  @member = Member.new(member_params)
+
+    if @member.save
+      redirect_to @member, notice: 'New member was successfully created.'
+    else
+      render action: 'new'
+    end
+end
+
+
+def update
+  if @member.update(member_params)
+    redirect_to @member
+  else
+    render action: 'edit'
+  end
+end
+
+def destroy
+  @member.destroy
+  redirect_to projections_path
+end
+
+private
+
+def set_member
+  @member = Member.find(params[:id])
+end
+
+def member_params
+  params.require(:member).permit(:name, :member_type, :dob, :bio)
+end
+
+end
---
+++ app/controllers/statements_controller.rb
@@ -0,0 +1,3 @@
+class StatementsController < ApplicationController
+
+end
