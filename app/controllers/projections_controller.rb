172c9ab1c29070b8c53adb3395c476eca537bf86
Jay Pinho <jaypinho@gmail.com>
2022-03-27 10:07:05 +0100

    Remove long-term projected rates from projection stats

--- app/controllers/projections_controller.rb
+++ app/controllers/projections_controller.rb
@@ -177,7 +177,7 @@ order by DaysUntil asc, ProjectionError asc
 
     json_response = {
       "actual_rates": KeyRate.all.order(rate_date: :asc),
-      "projected_rates": Projection.all.select('fulfillment_date, min(projected_rate), max(projected_rate), avg(projected_rate), PERCENTILE_CONT(0.5) WITHIN GROUP(ORDER BY projected_rate) As median, count(*)').group(:fulfillment_date).order(fulfillment_date: :asc),
+      "projected_rates": Projection.all.select('fulfillment_date, min(projected_rate), max(projected_rate), avg(projected_rate), PERCENTILE_CONT(0.5) WITHIN GROUP(ORDER BY projected_rate) As median, count(*)').where('fulfillment_date <> ?', LONG_RUN_PLACEHOLDER_DATE).group(:fulfillment_date).order(fulfillment_date: :asc),
       "projected_long_term_rates": Projection.all.select('present_date, min(projected_rate), max(projected_rate), avg(projected_rate), PERCENTILE_CONT(0.5) WITHIN GROUP(ORDER BY projected_rate) As median, count(*)').where(:fulfillment_date => LONG_RUN_PLACEHOLDER_DATE).group(:present_date).order(present_date: :asc),
       "projected_rates_detail": Projection.select('days_until, projection_error, sum(projection_count) As projection_total').from("(#{from_sql}) as aggregated_projected_and_actual_rates").group(:days_until, :projection_error).order('projection_total desc, days_until asc, projection_error asc')
     }
